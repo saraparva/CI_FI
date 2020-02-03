0d57eef9099e44872e5b0aa71f769f4aa7053e0a
saraparva <60186938+saraparva@users.noreply.github.com>
2020-02-03 19:45:28 +0100

    Add files via upload

---
+++ prueba.cpp
@@ -0,0 +1,1 @@
+#include <iostream>using namespace std;//nuevo tipo de dato//subprogramasconst float diasXanyos=365.25;int main(){    int edad,D;    cout<<"ÀQue edad tienes?";    cin>>edad;    D=edad*diasXanyos;    cout<<"Has vivido "<<D<<"dias"<<endl;}
