917885393eb225b80509f99b0e908c8a0c07e02d
saraparva <60186938+saraparva@users.noreply.github.com>
2020-03-03 13:01:59 +0100

    Update D4_Numero_cifras.cpp

--- D4_Numero_cifras.cpp
+++ D4_Numero_cifras.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int num, suma=0, cifra;
+    int num, suma=0;
     cout<<"Introduce un numero: ";
     cin>>num;
     while (num!=0){
-        cifra=num%10;
         suma++;
         
  //suma++ es igual que suma+=1 y que suma=suma+1
